1b2fa57987ee62235d19e74b9a41b90bc59ca3cf
Gustavo Ridgue <guferrari007@gmail.com>
2021-04-22 10:38:57 -0300

    Initialize api

---
+++ src/main/java/com/api/annote/AnnoteApplication.java
@@ -0,0 +1,13 @@
+package com.api.annote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnnoteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnnoteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/annote/AnnoteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.annote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnnoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
